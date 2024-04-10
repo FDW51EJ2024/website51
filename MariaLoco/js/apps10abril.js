0f6f8375baa72cb34d8b034a8ce079c63ee3014e
Javier Alejandro De La Garza Zambrano <1alexdelagarza@gmail.com>
2024-04-10 10:37:37 -0600

    agregar folders en clase

--- MariaLoco/js/apps10abril.js
+++ MariaLoco/js/apps10abril.js
@@ -1,1 +1,16 @@
+
 /* crear las referencias a los objetos que seran programados */
+
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const suma = document.getElementById("suma");
+const btnSumar = document.getElementById("btnSumar");
+
+
+btnSumar.addEventListener("click", e => {
+    e.preventDefault();
+
+    /* codigo para sumar */
+    let n1 = num1.value;
+    let n2 = num2.value;
+});
