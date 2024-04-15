4c730ec63edaa016dec3e140ef1825d8b198fbd3
Satou118 <matsuzu18@gmail.com>
2024-04-15 15:08:22 -0600

    App3 del 15 Abril Js

---
+++ Satou118/SatouJs/App3_15Abril.js
@@ -0,0 +1,14 @@
+const calif = document.getElementById(calif);
+const nota = document.getElementById(nota);
+const boton = document.getElementById(btnnota);
+
+boton.addEventListener("click", e =>{
+    e.preventDefault();
+
+    let excelente = 
+
+    if (calif = 100){
+
+        nota.value = excelente;
+    }
+});
