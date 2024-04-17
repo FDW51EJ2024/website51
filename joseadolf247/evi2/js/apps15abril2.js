410dcb5dfcbe36358a74afa066546fa668d86c00
Jose Adolfo Rodriguez Hernandez <joseadolfo.rdz@gmail.com>
2024-04-16 23:36:02 -0600

    se agregan aplicaciones de clase

---
+++ joseadolf247/evi2/js/apps15abril2.js
@@ -0,0 +1,23 @@
+const horas = document.getElementById("horas");
+const pago = document.getElementById("pago");
+const sueldo = document.getElementById("sueldo");
+const boton = document.getElementById("btnCalcular");
+
+boton.addEventListener("click", e => {
+    e.preventDefault();
+
+    let hrs = parseInt(horas.value);
+    let pagoxhr = parseFloat(pago.value);
+
+    if(hrs > 40){
+        let hrsExtra = hrs - 40;
+        let sueldoExtra = hrsExtra * (pagoxhr * 4);
+        let sueldoBase = 40 * pagoxhr
+        
+        sueldo.value = sueldoBase + sueldoExtra;
+    }
+    else{
+        let res = hrs * pagoxhr;
+        sueldo.value = res;
+    }
+});
---
+++ joseadolf247/evi2/js/apps15abril3.js
@@ -0,0 +1,23 @@
+const calificacion = document.getElementById("calificacion");
+const nota = document.getElementById("nota");
+const boton = document.getElementById("btnCalcular");
+
+boton.addEventListener("click", e => {
+    e.preventDefault();
+
+    let cali = parseInt(calificacion.value);
+
+    if(cali<=69){
+        nota.value = "Terceras";
+    } else if((cali==70) || (cali<=79)){
+        nota.value = "Regular";
+    } else if((cali==80) || (cali<=89)){
+        nota.value = "Bien";
+    } else if((cali==90) || (cali<=99)){
+        nota.value = "Muy Bien";
+    } else if(cali==100){
+        nota.value = "Excelente";
+    } else{
+        nota.value = "Calificacion no valida";
+    }
+});
