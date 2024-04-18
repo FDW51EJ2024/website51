66bb8db4827f7c86136c7ed4c83541deb809f48a
Emmanuel Cavazos <emmanuel.c.svdr@gmail.com>
2024-04-18 12:16:06 -0600

    app de clase

---
+++ EmmanuelCS04/appsJS/app17-04dos.js
@@ -0,0 +1,12 @@
+/* Mandar a la consola la suma de esta serie: 2000,2001+....+2150 */
+
+const boton = document.getElementById("boton");
+
+boton.addEventListener("click", e =>{
+    e.preventDefault()
+    let suma = 0
+    for (let num =2000; num<=2150; num++){
+        suma = suma + num
+    }
+    console.log("La suma es: ", suma);
+})
