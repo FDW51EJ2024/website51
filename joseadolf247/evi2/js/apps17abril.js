f70f456ac125ddd233c3e8bb0e27a277e7f799de
Jose Adolfo Rodriguez Hernandez <joseadolfo.rdz@gmail.com>
2024-04-18 23:29:18 -0600

    se agrega aplicacion de area y suma

---
+++ joseadolf247/evi2/js/apps17abril.js
@@ -0,0 +1,26 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const area = document.getElementById("area");
+const resultado = document.getElementById("resultado");
+const btncalcular = document.getElementById("btncalcular");
+
+btncalcular.addEventListener("click",e=> {
+    e.preventDefault();
+    let n1 = parseInt(num1.value);
+    let n2 = parseInt(num2.value);
+    let a = area.value; 
+    
+    switch (a){
+        case "triangulo":
+            let op1 = (n1 * n2) / 2;
+            resultado.value = op1;
+            break;
+        case  "cuadrado":
+            let op2 = n1 * n1;
+            resultado.value = op2;
+            break;
+        default:
+            resultado.value = "opcion no valida";
+            break;
+    }
+});
