b9af22aeb078d38fb8d4a3f52714ff0047370abf
Javier Alejandro De La Garza Zambrano <1alexdelagarza@gmail.com>
2024-04-22 11:38:57 -0600

    actividad completa

--- MariaLoco/js/apps22abril3.js
+++ MariaLoco/js/apps22abril3.js
@@ -1,3 +1,35 @@
 /*es primo si solamente tiene 2 divisores
 1 y el mismo numero EJEMPLO: 3, 5, 7
 if(numero%1==0 && numero%numero==0) NO JALA*/
+const inicio = document.getElementById("inicio");
+const fin = document.getElementById("fin");
+const boton = document.getElementById("btnVer");
+const lista = document.getElementById("lista");
+
+boton.addEventListener("click", e => {
+    e.preventDefault();
+    let ini = parseInt(inicio.value);
+    let f = parseInt(fin.value);
+
+    lista.innerHTML = "";
+
+    for (let i = ini; i <= f; i++) {
+        let isPrime = true;
+        if (i > 1) {
+            for (let j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        } else {
+            isPrime = false;
+        }
+
+        if (isPrime) {
+            let item = document.createElement("li");
+            item.textContent = i;
+            lista.appendChild(item);
+        }
+    }
+});
