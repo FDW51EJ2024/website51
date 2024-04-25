c24c4c5e5107f6b666e00eded68d31974b1ac133
Fjazmin <jazmin.hernandezf@uanl.edu.mx>
2024-04-24 20:05:14 -0600

    Cambios Realizados

---
+++ FJazmin/Evidencia2/AppJS/22abrilTarea.js
@@ -0,0 +1,60 @@
+const inicio = document.getElementById("inicio");
+const fin = document.getElementById("fin");
+const orden = document.getElementById("orden");
+const boton = document.getElementById("boton");
+const lista = document.getElementById("lista");
+
+boton.addEventListener("click", e => {
+    e.preventDefault();
+    let ini = parseInt(inicio.value);
+    let f = parseInt(fin.value);
+    let o = orden.value;
+
+    if (o == "A") {
+        let i = ini;
+        lista.innerHTML = "";
+        do {
+            let esPrimo = true;
+            if (i <= 1) {
+                esPrimo = false;
+            } else {
+                for (let j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j === 0) {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (esPrimo) {
+                let item = document.createElement("li");
+                item.innerHTML = i;
+                lista.appendChild(item);
+            }
+            i++;
+        } while (i <= f);
+    } else if (o == "D") {
+        let i = f;
+        lista.innerHTML = "";
+        do {
+            let esPrimo = true;
+            if (i <= 1) {
+                esPrimo = false;
+            } else {
+                for (let j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j === 0) {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (esPrimo) {
+                let item = document.createElement("li");
+                item.innerHTML = i;
+                lista.appendChild(item);
+            }
+            i--;
+        } while (i >= ini);
+    }
+});
